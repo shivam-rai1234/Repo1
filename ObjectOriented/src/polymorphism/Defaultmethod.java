16e2daee9e05ba324abb9b59ede346bc44b56062
shivam-rai1234 <shivam.rai@kpipartners.com>
2022-09-28 18:00:58 +0530

    commit default method

--- ObjectOriented/src/polymorphism/Defaultmethod.java
+++ ObjectOriented/src/polymorphism/Defaultmethod.java
@@ -8,22 +8,24 @@ interface Checkout{
 	{	int sum=a+b;
 		System.out.println("after sum = "+sum);
 	}
-	
-	
 }
-
 public class Defaultmethod implements Checkout{
-
+	
+	
 	@Override
 	public void num(int a) {
 		// TODO Auto-generated method stub
 		
 	}
+
+	
 	public static void main(String args[])
 	{
 		Defaultmethod d=new Defaultmethod();
 		d.num(4);
 		d.add(2,3);
 	}
+
+	
 
 }
